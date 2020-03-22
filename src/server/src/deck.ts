fdd03409b57f39ed3570c2f4be18057309081833
Austin Berke <austinberke@gmail.com>
2020-03-22 11:38:23 -0700

    Updates

--- src/server/src/deck.ts
+++ src/server/src/deck.ts
@@ -1,27 +1,27 @@
 export class Deck {
-    deck: number[];
+  deck: number[];
 
-    constructor() {
-        this.deck = Array.from(new Array(52), (x, i) => i)
-        this.shuffle()
-    }
+  constructor() {
+    this.deck = Array.from(new Array(52), (x, i) => i);
+    this.shuffle();
+  }
 
-    print(): void {
-        console.log(this.deck)
-    }
+  print(): void {
+    console.log(this.deck);
+  }
 
-    // Stack Shuffle
-    shuffle(): void {
-        let count = this.deck.length;
-        while(count) {
-            this.deck.push(this.deck.splice(Math.floor(Math.random() * count), 1)[0]);
-            count -= 1;
-        }
+  // Stack Shuffle
+  shuffle(): void {
+    let count = this.deck.length;
+    while (count) {
+      this.deck.push(this.deck.splice(Math.floor(Math.random() * count), 1)[0]);
+      count -= 1;
     }
+  }
 
-    draw(): number {
-        return this.deck.pop() as number;
-    }
+  draw(): number {
+    return this.deck.pop() as number;
+  }
 }
 
 export default Deck;
