d7826e3320cdd56ef541ebd80630ea66733b1f33
Jacob Fisher <jacobfisher18@gmail.com>
2020-03-22 17:27:29 -0700

    added Card class for displaying cards on the front end

---
+++ src/client/src/utilities/constants.js
@@ -0,0 +1,22 @@
+export const RANKS = {
+  ACE: 'ACE',
+  TWO: 'TWO',
+  THREE: 'THREE',
+  FOUR: 'FOUR',
+  FIVE: 'FIVE',
+  SIX: 'SIX',
+  SEVEN: 'SEVEN',
+  EIGHT: 'EIGHT',
+  NINE: 'NINE',
+  TEN: 'TEN',
+  JACK: 'JACK',
+  QUEEN: 'QUEEN',
+  KING: 'KING',
+}
+
+export const SUITS = {
+  CLUBS: 'CLUBS',
+  DIAMONDS: 'DIAMONDS',
+  SPADES: 'SPADES',
+  HEARTS: 'HEARTS',
+}
