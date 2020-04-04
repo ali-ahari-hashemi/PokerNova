873fe9cfbb44030322290fd9de6bfef4f394c959
Ali Hashemi <alihashe@usc.edu>
2020-04-03 22:08:56 -0700

    use while loop instead of recursive function for generating ids

--- src/server/src/utils/getUniqueId.ts
+++ src/server/src/utils/getUniqueId.ts
@@ -2,9 +2,9 @@ import Game from '../classes/Game';
 import { v1 as uuid } from 'uuid';
 
 export const getUniqueId = (games: Map<string, Game>): string => {
-  const id: string = uuid().substr(0, 4);
-  if (games.has(id)) {
-    getUniqueId(games);
-  }
+  let id: string;
+  do {
+    id = uuid().substr(0, 4);
+  } while (games.has(id));
   return id;
 };
