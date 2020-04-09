2aeaf1e0633aced45ec11dcdea0fe283ea777e56
Ali Hashemi <alihashe@usc.edu>
2020-04-08 21:10:43 -0700

    added player status bar

--- src/client/src/components/PlayerModule.jsx
+++ src/client/src/components/PlayerModule.jsx
@@ -5,13 +5,18 @@ function PlayerModule(props) {
   const { player, status, total, active } = props;
 
   return (
-    <div className={`PlayerModule ${active && "PlayerModuleActive"}`}>
-      <div className="PlayerModuleTotalContainer">
-        <p className="PlayerModuleTotalText">${total}</p>
+    <div className="Container">
+      <div className={`PlayerModule ${active && 'PlayerModuleActive'}`}>
+        <div className="PlayerModuleTotalContainer">
+          <p className="PlayerModuleTotalText">${total}</p>
+        </div>
+        <div className="PlayerModuleDetailsContainer">
+          <p className="PlayerModulePlayerText OneLine">{player}</p>
+        </div>
       </div>
-      <div className="PlayerModuleDetailsContainer">
-        <p className="PlayerModulePlayerText OneLine">{player}</p>
-        <p className="PlayerModuleStatusText OneLine">{status}</p>
+
+      <div className="StatusBar">
+        <p className="StatusText">{status}</p>
       </div>
     </div>
   );
